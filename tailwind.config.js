fd69e4b0012d0e668c7f6e4f29d6ec1ad0bc16e4
Royce551 <Royce551@users.noreply.github.com>
2024-12-18 20:03:54 +0000

    deploy: be9bc7175bab929b351a6deb3eaa3de56a5d4fc5

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    content: [
+        './_drafts/**/*.html',
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.md',
+        './*.html',
+        './FRESHMusicPlayer/*.html'
+    ],
+    theme: {
+        colors: {
+            'primarytext': '#000000',
+            'secondarytext': '#525252',
+            'foreground': '#E9EAED',
+            'secondary': '#F4F4F6',
+            'background': '#FFFFFF',
+            'control': '#E0E0E0',
+            'blue': '#338BC1',
+            'darkblue': '#13293D',
+            'green': '#69B578',
+            'red': '#d5463f',
+            'dark-primarytext': '#FFFFFF',
+            'dark-secondarytext': '#DADADA',
+            'dark-foreground': '#22252A',
+            'dark-secondary': '#16191D',
+            'dark-background': '#0B0D0E',
+            'dark-control': '#686F73',
+        },
+        fontFamily: {
+            sans: ['Source Sans 3', 'sans-serif'],
+        },
+        fontSize: {
+            sm: '12px',
+            base: '15px',
+            md: '24px',
+            'xl': '32px',
+            '2xl': '48px',
+        },
+        extend: {
+            fontFamily: {
+                sourcesans: ['"Source Sans 3"']
+            }
+        }
+    },
+
+    plugins: []
+}
